21048f868573c74b29a03c05963f0e9a805c7a46
andreasbundgaard <andreasb94@live.dk>
2015-01-20 11:22:06 +0100

    sqltry

--- Project X/MainWindow.xaml.cs
+++ Project X/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
 
 namespace Project_X {
     /// <summary>
@@ -20,6 +21,28 @@ namespace Project_X {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+        }
+
+        public void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            string connectionString = "Server=ealdb1.eal.local; User ID=ejl15_usr; Password=Baz1nga15; Database=EJL15_DB";
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+
+                    string query =
+                    "UPDATE aspnet_PersonalInformation Set P_Name=@personName, P_CPRNR=@cprPerson";
+
+                    SqlCommand cmd = new SqlCommand(query, con);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                
+            }
         }
     }
 }
